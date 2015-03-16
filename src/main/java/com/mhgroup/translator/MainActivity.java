9c5f2b24bdea26c734e91ed2324d4cb05f08a6be
wangdingkang <1257332448@qq.com>
2015-03-16 18:17:24 -0400

    new version

--- src/main/java/com/mhgroup/translator/MainActivity.java
+++ src/main/java/com/mhgroup/translator/MainActivity.java
@@ -4,14 +4,65 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.mhgroup.reader.MyReader;
+import com.mhgroup.reader.MyTranslator;
+import com.mhgroup.util.PromptUtils;
+
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private MyReader reader = null;
+    private MyTranslator translator = null;
+
+    private Button playButton = null;
+
+    private EditText translatedET = null, originalET = null;
+    private String translatedText = null;
+
+    private Button.OnClickListener buttonClickListener = new Button.OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId())
+            {
+                case R.id.playButton:
+                {
+                    String text = translatedET.getText().toString();
+                    reader.read(text);
+                }
+
+                default:
+                {
+
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PromptUtils.currentContext = MainActivity.this;
+
+        // We need to change the locale later.
+        reader = new MyReader(MainActivity.this, Locale.getDefault());
+        translator = new MyTranslator(Locale.getDefault(), Locale.getDefault());
+
+        translatedText = new String();
+
+        originalET = (EditText)findViewById(R.id.originalEditText);
+        translatedET = (EditText)findViewById(R.id.translatedEditText);
+
+        playButton = (Button)findViewById(R.id.playButton);
+        playButton.setOnClickListener(buttonClickListener);
     }
 
 
@@ -36,4 +87,19 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void startTranslate()
+    {
+        String originalText = originalET.getText().toString();
+        if(!originalText.equals(""))
+        {
+            translatedText = translator.translate(originalText);
+            translatedET.setText(translatedText);
+        }
+        else
+        {
+            PromptUtils.showMessage("No input.");
+        }
+    }
+
 }
